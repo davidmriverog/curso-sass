38302109354a317acd7b8d7c3d914c18f16415fd
David M. Rivero G <davidmriverog@gmail.com>
2016-04-15 16:04:07 -0430

    avances realizados en el ejercicio nro. 1

--- eje_001/gulpfile.js
+++ eje_001/gulpfile.js
@@ -1,5 +1,34 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-    console.log('Hola Mundo!!!');
+var gulp       = require('gulp'),
+    sass       = require('gulp-sass'),
+    cssmin     = require('gulp-cssmin'),
+    shell      = require('gulp-shell'),
+    rename     = require('gulp-rename');
+    
+
+// define the default task and add the watch task to it
+gulp.task('default', ['build-css','build-cssmin']);
+
+gulp.task('build-css',function(){
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build-cssmin',function(){
+  return gulp.src('dist/css/styles.css')
+    .pipe(cssmin())
+    .pipe(rename({suffix:'.min'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+/*
+gulp.task('cleanCMD', shell.task([
+  'cd dist/css',
+  'rm -f styles.css'
+])); */
+
+gulp.task('watch',function(){
+  gulp.watch('src/scss/**/*.scss',['build-css']);
 });
