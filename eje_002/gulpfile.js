9a73b17590951d0bb90e5338307296ce5d59139c
David M. Rivero G <davidmriverog@gmail.com>
2016-04-18 14:29:36 -0430

    ejercicio nro.2, uso de variables en sass.

--- eje_002/gulpfile.js
+++ eje_002/gulpfile.js
@@ -3,6 +3,7 @@
 var gulp       = require('gulp'),
     sass       = require('gulp-sass'),
     cssmin     = require('gulp-cssmin'),
+    concatCss  = require('gulp-concat-css'),
     rename     = require('gulp-rename');
     
 
@@ -10,13 +11,14 @@ var gulp       = require('gulp'),
 gulp.task('default', ['build-css']);
 
 gulp.task('build-css',function(){
-  return gulp.src('src/scss/**/*.scss')
+  return gulp.src(['src/scss/**/*.scss','src/scss/**/*.sass'])
     .pipe(sass())
+    .pipe(concatCss('theme.css'))
     .pipe(cssmin())
     .pipe(rename({suffix:'.min'}))
     .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('watch',function(){
-  gulp.watch('src/scss/**/*.scss',['build-css']);
+  gulp.watch(['src/scss/**/*.scss','src/scss/**/*.sass'],['build-css']);
 });
